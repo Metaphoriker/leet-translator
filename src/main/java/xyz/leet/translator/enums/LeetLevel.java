4b0e768d54b5809346a37b8100858402e809f4c1
Luziferium <luziferisdoof@gmail.com>
2021-08-23 10:57:59 +0200

    Added Encryption Code

--- src/main/java/xyz/leet/translator/enums/LeetLevel.java
+++ src/main/java/xyz/leet/translator/enums/LeetLevel.java
@@ -2,11 +2,20 @@ package xyz.leet.translator.enums;
 
 public enum LeetLevel {
 
-    LEET_LEVEL_1,
-    LEET_LEVEL_2,
-    LEET_LEVEL_3,
-    EMOJI,
-    CAESAR_SHIFT,
-    FUCKERY;
+    LEET_LEVEL_1("L1"),
+    LEET_LEVEL_2("L2"),
+    LEET_LEVEL_3("L3"),
+    EMOJI("E"),
+    CAESAR_SHIFT("CS"),
+    FUCKERY("F");
 
+    private final String encryptionCode;
+
+    LeetLevel(String encryptionCode) {
+        this.encryptionCode = encryptionCode;
+    }
+
+    public String getEncryptionCode() {
+        return encryptionCode;
+    }
 }
