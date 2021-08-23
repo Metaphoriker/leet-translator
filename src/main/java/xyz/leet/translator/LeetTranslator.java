30734ad4d56073a58ea898f209c15c7cacc39320
olijeffers0n <oliverarnup@hotmail.com>
2021-08-23 11:02:42 +0100

    its a monster

--- src/main/java/xyz/leet/translator/LeetTranslator.java
+++ src/main/java/xyz/leet/translator/LeetTranslator.java
@@ -30,13 +30,59 @@ public class LeetTranslator {
 
         StringBuilder builder = new StringBuilder();
 
-        for(int i = 0; i != leet.length(); i++) {
+        String[] leetWords = leet.split(" ");
 
-            String current = String.valueOf(leet.charAt(i));
+        for (String word : leetWords) {
 
-            if(Character.isDigit(leet.charAt(i)))
-                builder.append(LeetLetter.fromLeet(current).getLetter());
-            else if(!Character.isAlphabetic(leet.charAt(i))) builder.append(" ");
+            StringBuilder translatedWord = new StringBuilder();
+
+            int evaluated = word.length();
+            int loopsToSkip = 0;
+
+            for(int i = word.length() -1; i != -1; i--) {
+
+                if (loopsToSkip != 0) {
+                    evaluated += 1;
+                    loopsToSkip -= 1;
+                    continue;
+                }
+
+                String currentString = word.substring(i, evaluated);
+                if (currentString.equalsIgnoreCase(",") || currentString.equalsIgnoreCase(".")) {
+                    translatedWord.insert(0, currentString);
+                    evaluated = i;
+                    continue;
+                }
+                LeetLetter fromLeet = LeetLetter.fromLeet(currentString);
+
+                if (fromLeet != LeetLetter.DEFAULT) {
+                    String letter = fromLeet.name();
+
+                    try {
+                        if (letter.equalsIgnoreCase("V")) {
+
+                                if (LeetLetter.fromLeet(word.substring(i - 2, evaluated)).name().equalsIgnoreCase("W")) {
+                                    letter = "W";
+                                    loopsToSkip += 2;
+                                }
+                                if (LeetLetter.fromLeet(word.substring(i - 1, evaluated)).name().equalsIgnoreCase("N")) {
+                                    letter = "N";
+                                    loopsToSkip += 1;
+                                }
+
+                        }else if (letter.equalsIgnoreCase("I")) {
+
+                                String substring = word.substring(i - 1, evaluated - 1);
+                                if (LeetLetter.fromLeet(substring).name().equalsIgnoreCase("I")) letter = "L";
+
+                        }
+                    }catch (StringIndexOutOfBoundsException ignored) {}
+
+                    translatedWord.insert(0, letter);
+                    evaluated = i;
+                }
+            }
+            builder.append(translatedWord).append(" ");
         }
         return builder.toString();
     }
