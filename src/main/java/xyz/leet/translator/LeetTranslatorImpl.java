18a0e46539ccbed6c595b60b96d468a1e6f2fd8a
Luziferium <luziferisdoof@gmail.com>
2021-08-24 15:00:15 +0200

    Implemented #convert method

--- src/main/java/xyz/leet/translator/LeetTranslatorImpl.java
+++ src/main/java/xyz/leet/translator/LeetTranslatorImpl.java
@@ -1,30 +1,45 @@
 package xyz.leet.translator;
 
+import xyz.leet.translator.converter.CaesarShiftConverter;
+import xyz.leet.translator.converter.EmojiConverter;
+import xyz.leet.translator.converter.LeetConverter;
 import xyz.leet.translator.enums.EncryptionType;
 import xyz.leet.translator.enums.Letter;
 
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LeetTranslatorImpl implements LeetTranslator {
 
     @Override
     public String convert(Letter letter, EncryptionType encryptionType) {
-        return null;
+
+        //TODO hardcoded
+        int shift = ThreadLocalRandom.current().nextInt(5)+1;
+        return switch (encryptionType) {
+
+            case LEET_LEVEL_1 -> addEncryptionCode(toLeet(letter, 1), encryptionType);
+            case LEET_LEVEL_2 -> addEncryptionCode(toLeet(letter, 2), encryptionType);
+            case LEET_LEVEL_3 -> addEncryptionCode(toLeet(letter, 3), encryptionType);
+            case EMOJI -> addEncryptionCode(toEmoji(letter), encryptionType);
+            case CAESAR_SHIFT -> addEncryptionCode(shift+toCaesarShift(letter, shift), encryptionType);
+            case FUCKERY -> throw new UnsupportedOperationException("Not implemented yet");
+        };
     }
 
     @Override
     public String toLeet(Letter letter, int level) {
-        return null;
+        return LeetConverter.convert(letter, level);
     }
 
     @Override
     public String toCaesarShift(Letter letter, int shift) {
-        return null;
+        return CaesarShiftConverter.convert(letter, shift);
     }
 
     @Override
     public String toEmoji(Letter letter) {
-        return null;
+        return EmojiConverter.convert(letter);
     }
 
     @Override
@@ -32,6 +47,9 @@ public class LeetTranslatorImpl implements LeetTranslator {
         return null;
     }
 
+    /*
+     * NOTE: String#startsWith(String) check at the beginning of CaesarShift String for "deshifting"
+     */
     @Override
     public Letter toLetter(String string) {
         return null;
