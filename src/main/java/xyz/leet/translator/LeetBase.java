34de8b7e0148b5e68f661e4cbb613d532b8cd4f8
Luziferium <luziferisdoof@gmail.com>
2021-08-25 14:25:03 +0200

    Added UpdateChecker

--- src/main/java/xyz/leet/translator/LeetBase.java
+++ src/main/java/xyz/leet/translator/LeetBase.java
@@ -3,15 +3,23 @@ package xyz.leet.translator;
 import xyz.leet.translator.gui.LeetGUI;
 import xyz.leet.translator.translator.LeetTranslator;
 import xyz.leet.translator.translator.LeetTranslatorImpl;
+import xyz.leet.translator.version.UpdateChecker;
+
+import javax.swing.*;
+import java.util.concurrent.CompletableFuture;
 
 public class LeetBase {
 
     private static final LeetTranslator LEET_TRANSLATOR = new LeetTranslatorImpl();
+    private static final UpdateChecker UPDATE_CHECKER = new UpdateChecker();
 
     public static void main(String[] args) {
 
         LeetGUI leetgui = new LeetGUI(LEET_TRANSLATOR);
         leetgui.buildGUI();
+
+        CompletableFuture.supplyAsync(UPDATE_CHECKER::isUpdateAvailable)
+            .thenAccept(b -> JOptionPane.showMessageDialog(leetgui, "An update is available \n http://leet-translator.xyz/", "Update available", JOptionPane.INFORMATION_MESSAGE));
     }
 
 }
---
+++ src/main/java/xyz/leet/translator/version/UpdateChecker.java
@@ -0,0 +1,68 @@
+package xyz.leet.translator.version;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class UpdateChecker {
+
+    private static final String URL_LINK = "https://raw.githubusercontent.com/redaam/leet-translator/main/src/main/resources/xyz/leet/translator/version.txt?token=AL5WWYJ7EHRURRTI4QNB3JDBEYZXU";
+    private File versionFile;
+
+    public UpdateChecker() {
+
+        URL resource = getClass().getClassLoader().getResource("version.txt");
+
+        if(resource == null) {
+
+            try {
+                throw new FileNotFoundException("");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
+        instantiateVersionFile(resource);
+    }
+
+    public boolean isUpdateAvailable() {
+
+        HttpURLConnection connection = null;
+        String currentVersion;
+        String latestVersion;
+
+        try {
+
+            connection = (HttpURLConnection) new URL(URL_LINK).openConnection();
+            connection.connect();
+
+            currentVersion = Files.readString(Path.of(versionFile.toURI()));
+            latestVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return false;
+        } finally {
+
+            if(connection != null)
+                connection.disconnect();
+        }
+
+        return !latestVersion.equals(currentVersion);
+    }
+
+    private void instantiateVersionFile(URL resource) {
+
+        try {
+            this.versionFile = Paths.get(resource.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
