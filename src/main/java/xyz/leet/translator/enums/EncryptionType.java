2de3a6cddccf5a1ab4809ee805f111cff9415f2d
olijeffers0n <oliverarnup@hotmail.com>
2021-08-25 11:11:46 +0100

    Added `_` to the EncryptionType

--- src/main/java/xyz/leet/translator/enums/EncryptionType.java
+++ src/main/java/xyz/leet/translator/enums/EncryptionType.java
@@ -2,12 +2,12 @@ package xyz.leet.translator.enums;
 
 public enum EncryptionType {
 
-    LEET_LEVEL_1("L1"),
-    LEET_LEVEL_2("L2"),
-    LEET_LEVEL_3("L3"),
-    CAESAR_SHIFT("CS"),
-    FUCKERY("F"),
-    EMOJI("E");
+    LEET_LEVEL_1("_L1"),
+    LEET_LEVEL_2("_L2"),
+    LEET_LEVEL_3("_L3"),
+    CAESAR_SHIFT("_CS"),
+    FUCKERY("_F"),
+    EMOJI("_E");
 
     private final String encryptionCode;
 
