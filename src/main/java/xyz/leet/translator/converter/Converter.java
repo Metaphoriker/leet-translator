c8269ed187aabdc0858f7628339d0c07285fbe07
Luziferium <luziferisdoof@gmail.com>
2021-11-01 15:25:44 +0100

    Added important Javadoc

--- src/main/java/xyz/leet/translator/converter/Converter.java
+++ src/main/java/xyz/leet/translator/converter/Converter.java
@@ -8,12 +8,16 @@ import java.util.Optional;
 
 public abstract class Converter {
 
+    /**
+     * Tries to converts the given String to a single Letter.
+     * Does return an empty Optional if no matching Letter equivalents could be found.
+     */
     public abstract Optional<Letter> convert(String s);
 
     public abstract String convert(Letter l);
 
     /*
-     * This method is for override purpose only
+     * This method is for override-purpose only
      */
     public List<String> getFillers() {
         return Collections.emptyList();
