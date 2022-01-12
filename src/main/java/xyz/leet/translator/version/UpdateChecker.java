3d7833fd9c1d9fb609c6fb9da143acaba61ba771
Luziferium <luziferisdoof@gmail.com>
2022-01-12 16:10:57 +0100

    Fixed resource not found and therefore exceptions

--- src/main/java/xyz/leet/translator/version/UpdateChecker.java
+++ src/main/java/xyz/leet/translator/version/UpdateChecker.java
@@ -3,42 +3,31 @@ package xyz.leet.translator.version;
 import javafx.scene.control.Alert;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UpdateChecker {
     
     private static final String URL_LINK = "https://raw.githubusercontent.com/Luziferium/leet-translator/main/src/main/resources/xyz/leet/translator/version/version.txt";
     
-    private File versionFile;
+    private final String version;
     
     public UpdateChecker() {
         
-        URL resource = getClass().getClassLoader().getResource("xyz/leet/translator/version/version.txt");
-        
-        if (resource == null) {
+        InputStream resource = getClass().getClassLoader().getResourceAsStream("xyz/leet/translator/version/version.txt");
+    
+        if (resource == null)
             error("JAR is corrupted. Please reinstall or try again");
-            return;
-        }
-        
-        instantiateVersionFile(resource);
+    
+        this.version = fetchVersion(resource);
     }
     
     public UpdateCheckerResult checkUpdate() {
         
         HttpURLConnection connection = null;
-        String currentVersion;
         String latestVersion;
         
         try {
@@ -46,7 +35,6 @@ public class UpdateChecker {
             connection = (HttpURLConnection) new URL(URL_LINK).openConnection();
             connection.connect();
             
-            currentVersion = Files.readString(Path.of(versionFile.toURI()));
             latestVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
         
         } catch (Exception e) {
@@ -59,7 +47,7 @@ public class UpdateChecker {
             if (connection != null) connection.disconnect();
         }
         
-        return new UpdateCheckerResult(latestVersion, currentVersion, !latestVersion.equals(currentVersion));
+        return new UpdateCheckerResult(latestVersion, this.version, !latestVersion.equals(this.version));
     }
 
     private void error(String msg) {
@@ -70,23 +58,19 @@ public class UpdateChecker {
         alert.showAndWait();
     }
 
-    private Path fetchVersionFilePath(URI uri) throws IOException {
-    
-        Map<String, String> env = new HashMap<>();
+    private String fetchVersion(InputStream inputStream) {
         
-        String[] array = uri.toString().split("!");
-        FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-    
-        return fs.getPath(array[1]);
-    }
-    
-    private void instantiateVersionFile(URL resource) {
+        if(inputStream == null)
+            return ""; // avoid exceptions
         
-        try {
-            this.versionFile = new File(fetchVersionFilePath(resource.toURI()).toString());
-        } catch (URISyntaxException | IOException e) {
+        byte[] data = new byte[0];
+        try (InputStream in = inputStream) {
+            data = in.readAllBytes();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    
+        return new String(data);
     }
     
 }
